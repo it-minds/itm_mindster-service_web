899860fa8c95f78089abb52433d4a95d0e8a522c
mrb-itminds <78532121+mrb-itminds@users.noreply.github.com>
2021-03-04 14:28:45 +0100

    Form now properly posts a new service with a title and description
    Will add form validation next

--- frontend/src/components/Forms/Service/ServiceForm.tsx
+++ frontend/src/components/Forms/Service/ServiceForm.tsx
@@ -12,10 +12,35 @@ import {
   Wrap
 } from "@chakra-ui/react";
 import { useLocales } from "hooks/useLocales";
-import React, { FC } from "react";
+import React, { FC, useCallback, useState } from "react";
+import { genServiceClient } from "services/backend/apiClients";
+import { CreateServiceCommand } from "services/backend/nswagts";
 
 const ServiceForm: FC = () => {
   const { t } = useLocales();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = event => {
+    event.preventDefault();
+    addService();
+  };
+
+  const addService = useCallback(async () => {
+    const serviceClient = await genServiceClient();
+    console.log(title);
+    console.log(description);
+    await serviceClient.createService(
+      new CreateServiceCommand({
+        service: {
+          title: title,
+          description: description,
+          state: 0
+        }
+      })
+    );
+  }, [title, description]);
 
   return (
     <Center>
@@ -23,18 +48,27 @@ const ServiceForm: FC = () => {
         <Heading>Add new Service</Heading>
         <Flex width="full" align="center" justifyContent="center">
           <Box width="full" p={6} borderWidth={1} borderRadius={8} boxShadow="lg">
-            <FormControl isRequired>
-              <FormLabel>Title:</FormLabel>
-              <Input type="text"></Input>
-              <FormHelperText>Write the title for the new service</FormHelperText>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Description:</FormLabel>
-              <Textarea placeholder="Here is a sample placeholder" />
-            </FormControl>
-            <Button variant="outline" width="full" mt={4} type="submit">
-              Submit
-            </Button>
+            <form onSubmit={onSubmit}>
+              <FormControl isRequired>
+                <FormLabel>Title:</FormLabel>
+                <Input
+                  type="text"
+                  value={title}
+                  placeholder="Title of your service"
+                  onChange={event => setTitle(event.target.value)}></Input>
+              </FormControl>
+              <FormControl mt="5">
+                <FormLabel>Description:</FormLabel>
+                <Textarea
+                  placeholder="Description of service"
+                  value={description}
+                  onChange={event => setDescription(event.target.value)}
+                />
+              </FormControl>
+              <Button variant="outline" width="full" mt={5} type="submit">
+                Submit
+              </Button>
+            </form>
           </Box>
         </Flex>
       </Wrap>
