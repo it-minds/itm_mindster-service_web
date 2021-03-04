82aa5b6aa332fb4e2cba6d6d334ac3671866d197
mrb-itminds <78532121+mrb-itminds@users.noreply.github.com>
2021-03-04 12:44:39 +0100

    Added form layout

--- frontend/src/components/Forms/Service/ServiceForm.tsx
+++ frontend/src/components/Forms/Service/ServiceForm.tsx
@@ -1,4 +1,16 @@
-import { Center, Heading, Wrap } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  Textarea,
+  Wrap
+} from "@chakra-ui/react";
 import { useLocales } from "hooks/useLocales";
 import React, { FC } from "react";
 
@@ -8,7 +20,23 @@ const ServiceForm: FC = () => {
   return (
     <Center>
       <Wrap width="700px" justify="center">
-        <Heading>{t("example.title")}</Heading>
+        <Heading>Add new Service</Heading>
+        <Flex width="full" align="center" justifyContent="center">
+          <Box width="full" p={6} borderWidth={1} borderRadius={8} boxShadow="lg">
+            <FormControl isRequired>
+              <FormLabel>Title:</FormLabel>
+              <Input type="text"></Input>
+              <FormHelperText>Write the title for the new service</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Description:</FormLabel>
+              <Textarea placeholder="Here is a sample placeholder" />
+            </FormControl>
+            <Button variant="outline" width="full" mt={4} type="submit">
+              Submit
+            </Button>
+          </Box>
+        </Flex>
       </Wrap>
     </Center>
   );
