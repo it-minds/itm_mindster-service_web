b541c1be7a740e891b32bc9c323712e824843342
mrb-itminds <78532121+mrb-itminds@users.noreply.github.com>
2021-03-09 14:13:36 +0100

    Archiced old next pages for now, might be used later

--- frontend/src/pages/Service/[id].tsx
+++ frontend/src/pages/Service/[id].tsx
@@ -1,13 +1,10 @@
-import ActionForm from "components/Forms/Action/ActionForm";
+import ServiceTable from "components/ServiceTable/ServiceTable";
 import { Locale } from "i18n/Locale";
 import { GetStaticProps, NextPage } from "next";
 import { I18nProps } from "next-rosetta";
-import { genServiceClient } from "services/backend/apiClients";
-import { ServiceIdDto } from "services/backend/nswagts";
-import { logger } from "utils/logger";
 
 const IndexPage: NextPage = () => {
-  return <ActionForm serviceId={1} />;
+  return <ServiceTable></ServiceTable>;
 };
 
 export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context => {
@@ -18,31 +15,31 @@ export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context =
   return { props: { table } };
 };
 
-export async function getStaticPaths() {
-  // let data: ServiceIdDto[] = [];
+// export async function getStaticPaths() {
+//   let data: ServiceIdDto[] = [];
 
-  // try {
-  //   const serviceClient = await genServiceClient();
-  //   data = await serviceClient.getAllServices();
-  // } catch (err) {
-  //   logger.warn("todoItemClient.get Error", err);
-  //   console.log("failed fetch");
-  // }
+//   try {
+//     const serviceClient = await genServiceClient();
+//     data = await serviceClient.getAllServices();
+//   } catch (err) {
+//     logger.warn("todoItemClient.get Error", err);
+//     console.log("failed fetch");
+//   }
 
-  // const paths = data.map((Service: ServiceIdDto) => `/Service/${Service.id}`);
+//   const paths = data.map((Service: ServiceIdDto) => `/Service/${Service.id}`);
 
-  // GetstaticPaths currently doenst work as i want it. The above code gives the
-  // following error
-  //   {"message":"isomorphicEnvSettings - isBrowser","args":[false]}
-  // {"message":"todoItemClient.get Error","args":[{"message":"request to https://localhost:5001/api/Service failed, reason: unable to verify the first certificate","type":"system","errno":"UNABLE_TO_VERIFY_LEAF_SIGNATURE","code":"UNABLE_TO_VERIFY_LEAF_SIGNATURE"}]}
-  return {
-    paths: [
-      { params: { locale: "", id: "1" } },
-      { params: { locale: "en-US", id: "1" } },
-      { params: { id: "2" } } // See the "paths" section below
-    ],
-    fallback: true // See the "fallback" section below
-  };
-}
+//   GetstaticPaths currently doenst work as i want it. The above code gives the
+//   following error
+//     {"message":"isomorphicEnvSettings - isBrowser","args":[false]}
+//   {"message":"todoItemClient.get Error","args":[{"message":"request to https://localhost:5001/api/Service failed, reason: unable to verify the first certificate","type":"system","errno":"UNABLE_TO_VERIFY_LEAF_SIGNATURE","code":"UNABLE_TO_VERIFY_LEAF_SIGNATURE"}]}
+//   return {
+//     paths: [
+//       { params: { locale: "", id: "1" } },
+//       { params: { locale: "en-US", id: "1" } },
+//       { params: { id: "2" } } // See the "paths" section below
+//     ],
+//     fallback: true // See the "fallback" section below
+//   };
+// }
 
 export default IndexPage;
--- frontend/src/pages/ServiceForm/index.tsx
+++ frontend/src/pages/ServiceForm/index.tsx
@@ -1,10 +1,10 @@
-import ServiceForm from "components/Forms/Service/ServiceForm";
+import ServiceTable from "components/ServiceTable/ServiceTable";
 import { Locale } from "i18n/Locale";
 import { GetStaticProps, NextPage } from "next";
 import { I18nProps } from "next-rosetta";
 
 const IndexPage: NextPage = () => {
-  return <ServiceForm />;
+  return <ServiceTable />;
 };
 
 export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context => {
