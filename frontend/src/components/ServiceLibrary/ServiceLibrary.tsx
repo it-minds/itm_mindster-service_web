04857c845d720bcdccb769f11abd9fe42ca087ed
mrb-itminds <78532121+mrb-itminds@users.noreply.github.com>
2021-03-17 11:21:20 +0100

    Made the Grid Responsive

--- frontend/src/components/ServiceLibrary/ServiceLibrary.tsx
+++ frontend/src/components/ServiceLibrary/ServiceLibrary.tsx
@@ -9,7 +9,6 @@ import {
   VStack,
   Wrap
 } from "@chakra-ui/react";
-import ViewAllBtn from "components/Common/ViewAllBtn";
 import ServiceTableMenu from "components/ServiceTable/ServiceTableMenus/ServiceTableMenu";
 import { ServiceContext } from "contexts/ServiceContext";
 import React, { FC, useContext, useState } from "react";
@@ -23,8 +22,8 @@ const ServiceLibrary: FC = () => {
 
   return (
     <Center>
-      <Wrap justify="center">
-        <VStack>
+      <Wrap width="full" justify="center" margin="50">
+        <VStack width="full">
           <HStack mb="40px">
             <Heading>Service Library</Heading>
             <ServiceTableMenu></ServiceTableMenu>
@@ -33,22 +32,28 @@ const ServiceLibrary: FC = () => {
             <Box p="4">Services</Box>
             <Spacer />
             <Box onClick={() => setAllShown(!allShown)} p="4">
-              <ViewAllBtn totalItems={services.length}></ViewAllBtn>
+              {!allShown ? (
+                <Box cursor="pointer">View all ({services.length})</Box>
+              ) : (
+                <Box cursor="pointer">Hide all ({services.length})</Box>
+              )}
             </Box>
           </Flex>
-          {allShown ? (
-            <SimpleGrid columns={4} spacingX="20px" spacingY="50px">
-              {services.map((Service: ServiceIdDto) => (
-                <LibraryCard key={Service.id} service={Service}></LibraryCard>
-              ))}
-            </SimpleGrid>
-          ) : (
-            <SimpleGrid columns={4} spacingX="20px" spacingY="50px">
-              {services.slice(0, 4).map((Service: ServiceIdDto) => (
-                <LibraryCard key={Service.id} service={Service}></LibraryCard>
-              ))}
-            </SimpleGrid>
-          )}
+          <Flex width="full">
+            {allShown ? (
+              <SimpleGrid width="full" minChildWidth="300px" spacingX="20px" spacingY="50px">
+                {services.map((Service: ServiceIdDto) => (
+                  <LibraryCard key={Service.id} service={Service}></LibraryCard>
+                ))}
+              </SimpleGrid>
+            ) : (
+              <SimpleGrid width="full" minChildWidth="300px" spacingX="20px" spacingY="50px">
+                {services.slice(0, 5).map((Service: ServiceIdDto) => (
+                  <LibraryCard key={Service.id} service={Service}></LibraryCard>
+                ))}
+              </SimpleGrid>
+            )}
+          </Flex>
         </VStack>
       </Wrap>
     </Center>
